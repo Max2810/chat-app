f9bccbe2c2a158440fdd4e8a482f9e71fdbc1500
Maxime Féry <maxime.fery@outlook.fr>
2019-10-21 15:19:21 +0200

    Challenge reactState

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ import Contact from'./components/Contact.js';
 function App() {
   return (
     <div className="App">
-      <Contact Name= 'Caroline Garza' avatar= "https://randomuser.me/api/portraits/women/33.jpg" online/>
-      <Contact Name= 'Dale Sullivan' avatar= "https://randomuser.me/api/portraits/men/97.jpg" online/>
-      <Contact Name= 'Norma Watts' avatar= "https://randomuser.me/api/portraits/women/22.jpg" offline/>
+      <Contact Name= 'Caroline Garza' avatar= "https://randomuser.me/api/portraits/women/33.jpg" />
+      <Contact Name= 'Dale Sullivan' avatar= "https://randomuser.me/api/portraits/men/97.jpg" />
+      <Contact Name= 'Norma Watts' avatar= "https://randomuser.me/api/portraits/women/22.jpg" />
+      <Contact Name= 'Paula Lawrence' avatar= "https://randomuser.me/api/portraits/women/48.jpg" />
     </div>
   );
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,19 +1,28 @@
 import React from 'react';
 import './Contact.css';
 
-const Contact = props => (
+class Contact extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+       isConnected : false 
+    }
+  }
+  render(){
+    return(
   <div className= "Contact">
-    <img className = "avatar" src={props.avatar} alt= 'avatar'/>
+    <img className = "avatar" src={this.props.avatar} alt= 'avatar'/>
       <div>
-        <h3 className="name">{props.Name}</h3>
-      <div className="status">
-        <div className= {props.online ? 'status-online' : 'status-offline'}></div>
-        <p className= 'status-text'>{props.online ? "online" : "offline"}</p>
+        <h3 className="name">{this.props.Name}</h3>
+      <div onClick = {event =>{const newState = !this.state.isConnected; this.setState({isConnected : newState})}} className="status">
+        <div className= {this.state.isConnected ? 'status-online' : 'status-offline'}></div>
+        <p className= 'status-text'>{this.state.isConnected ? "online" : "offline"}</p>
         </div>
       </div>
   </div>
  );
-
+}
+}
 
 
 export default Contact;
